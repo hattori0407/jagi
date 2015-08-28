c8b580e4ec53e614a260a1aabd63c4cc13e9434d
yinaura <yinaura@aiming-inc.com>
2015-08-28 18:57:14 +0900

    回答後のユーザープロフィールの表示処理で、flashではuser_profile.id だけを渡すようにした

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -7,6 +7,7 @@ class QuizzesController < ApplicationController
     @total_incorrect = current_user.user_profile.total_incorrect
 
     @profile_image_normal = ProfileImage.find_by(user_profile_id: @answer_user_profile.id, situation: 'normal') if @answer_user_profile.present?
+    @prev_answer_user_profile = UserProfile.find(flash[:prev_answer_user_profile_id]) if flash[:prev_answer_user_profile_id];
   end
 
   def answer
@@ -24,12 +25,7 @@ class QuizzesController < ApplicationController
       flash[:notice] = I18n.t("quiz.show.incorrect_result")
     end
 
-    flash[:name] = @answer_user_profile.name
-    flash[:answer_name] = @answer_user_profile.answer_name if @answer_user_profile.answer_name.present?
-    flash[:project] = @answer_user_profile.project.name if @answer_user_profile.project.present?
-    flash[:group] = @answer_user_profile.group.name if @answer_user_profile.group.present?
-    flash[:joined_year] = @answer_user_profile.joined_year if @answer_user_profile.joined_year.present?
-    flash[:detail] = @answer_user_profile.detail if @answer_user_profile.detail.present?
+    flash[:prev_answer_user_profile_id] = @answer_user_profile.id
 
     redirect_to quiz_path(params.permit(:joined_year, :group_id, :project_id, :review_mode, :joined_year) )
   end
